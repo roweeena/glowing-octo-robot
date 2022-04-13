2291cf94fb5e56819f2d6f2a420c59463a396ed2
roweeena <roe0320@gmail.com>
2022-04-14 00:38:03 +1000

    connected to atlas

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require ('express');
 const cors = require ('cors');
+const dotenv = require ('dotenv');
 const mongoose = require ('mongoose');
 const bodyParser = require('body-parser');
 
-const port = 3000;
+dotenv.config()
+mongoose.Promise = global.Promise;
+// mongoose.set('useFindAndModify', false);
+mongoose.connect(
+  `mongodb+srv://admin:${process.env.MONGO_PW}@cluster0.cmnvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  {useNewUrlParser: true}
+)
 
+const port = 3000;
 const app = express();
 
 app.listen(port);
 
+ 
+//404 page
+app.use((req,res) =>{
+    res.status(404).send({url:`${req.originalUrl} not found`})
+})
 app.get('/', (req, res) => {
     res.send ('Hello World');
 });
