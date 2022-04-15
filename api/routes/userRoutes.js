87cb9b70082587ae671e5e0114c5cbf75d9d6957
Alex King <calicoalix@gmail.com>
2022-04-15 21:40:55 +1000

    finished user routes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,1 +1,14 @@
 //TODO: route and export user routes from controller 
+
+import * as controller from '../controllers/user.js';
+import express from 'express';
+import { userAuth } from '../middleware/userAuth.js';
+
+const router = express.Router();
+
+router.get('/in', userAuth, controller.in); // need to rename this <-- auth user
+router.get('/:id', controller.getUser); // <-- getting user info
+router.get('/trips', controller.trips); // <-- getting all trips
+router.post('/signup', controller.signup); 
+router.post('/login', controller.login);
+router.post('/createTrip', controller.createTrip);
