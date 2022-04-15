619badf1b4385117f547c97833cfa335ef688829
Alex King <calicoalix@gmail.com>
2022-04-15 21:29:33 +1000

    set up routes in server

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const cors = require ('cors');
 const dotenv = require ('dotenv');
 const mongoose = require ('mongoose');
 const bodyParser = require('body-parser');
-
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const jwtAuthenticate = require('express-jwt')
+const userRouter = require('.routes/userRoutes.js')
+const tripRouter = require('.routes/tripRoutes.js')
 
 dotenv.config()
 mongoose.Promise = global.Promise;
@@ -15,13 +13,13 @@ mongoose.connect(
   `mongodb+srv://admin:${process.env.MONGO_PW}@cluster0.cmnvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   {useNewUrlParser: true}
 )
+const db = mongoose.connection 
 
 const port = 3000;
 const app = express();
 
 app.listen(port);
 
- 
 //404 page
 app.use((req,res) =>{
     res.status(404).send({url:`${req.originalUrl} not found`})
@@ -29,7 +27,11 @@ app.use((req,res) =>{
 app.get('/', (req, res) => {
     res.send ('Hello World');
 });
-console.log(`Server started on http://localhost:${port}`)
+// console.log(`Server started on http://localhost:${port}`)
+
+app.use('/user', userRouter);
+app.use('/event', tripRouter);
+
 
 // hi
 
